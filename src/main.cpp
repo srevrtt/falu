fecd4d0482b8a6d906bb1dc2ea44b41e5f262550
srevrtt <everettsistrunk@icloud.com>
2023-05-29 13:37:11 -0500

    Parser

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 
 #include "include/util.hpp"
 #include "include/lexer.hpp"
+#include "include/parser.hpp"
 
 int main(int argc, char *argv[]) {
     std::string luaSrc = Utilities::readLuaSrc("tests/hello_world.lua");
     std::vector<Token> tokens = Lexer::tokenize(luaSrc);
+    Node parseTree = Parser::parse(tokens);
 
     return 0;
 }
---
+++ src/parser.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+#include "include/parser.hpp"
+
+int parserIdx;
+std::vector<Token> tkns;
+
+static Node genNode() {
+    Node node;
+
+    if (tkns[parserIdx].name == "$T_LETTERS") {
+        parserIdx++;
+
+        // Function call
+        if (tkns[parserIdx].name == "$T_LPAREN") {
+            node.name = "fn:" + tkns[parserIdx - 1].value;
+
+            Node lparen;
+            lparen.name = "(";
+            node.nodes.push_back(lparen);
+
+            while (parserIdx < tkns.size() - 1 && tkns[parserIdx].name != "$T_RPAREN") {
+                Node param;
+                param.name = tkns[parserIdx].value;
+                node.nodes.push_back(param);
+
+                parserIdx++;
+            }
+
+            if (parserIdx < tkns.size() - 1) {
+                Node rparen;
+                rparen.name = ")";
+                node.nodes.push_back(rparen);
+            }
+        }
+    }
+
+    return node;
+}
+
+Node Parser::parse(std::vector<Token> tokens) {
+    tkns = tokens;
+    parserIdx = 0;
+
+    Node tree;
+    tree.name = "prog";
+    tree.nodes.push_back(genNode());
+
+    return tree;
+}
